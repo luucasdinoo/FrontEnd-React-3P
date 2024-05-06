55f7299152a1892cad4eb9262f860770f1c777d5
Lucas Bernadino <luucasdinoo@gmail.com>
2024-05-06 20:52:35 -0300

    Dashboard

---
+++ src/pages/dashboard/components/HeaderBar/index.tsx
@@ -0,0 +1,9 @@
+import * as S from './styled'
+
+export const HeaderBar: React.FC = () => {
+    return (
+        <S.HeaderWrapper>
+
+        </S.HeaderWrapper>
+    )
+}
---
+++ src/pages/dashboard/index.tsx
@@ -0,0 +1,9 @@
+import { HeaderBar } from "./components/HeaderBar"
+
+export const Dashboard: React.FC = () => {
+  return (
+    <>
+      <HeaderBar />
+    </>
+  )
+}
